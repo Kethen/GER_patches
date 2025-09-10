5618e6bbe7a62eec3201e80b0f88103276ced01e
Katharine Chui <katharine.chui@gmail.com>
2025-09-10 23:01:10 +0200

    tuning

--- main.c
+++ main.c
@@ -55,8 +55,16 @@ static void busynanosleep(uint64_t sleep_time_ns){
 		if (begin.tv_sec != now.tv_sec || now.tv_nsec - begin.tv_nsec > sleep_time_ns){
 			break;
 		}
-		//Sleep(0);
+		Sleep(0);
 	}
+}
+
+static void normalnanosleep(uint64_t sleep_time_ns){
+	struct timespec sleep_time = {
+		.tv_sec = 0,
+		.tv_nsec = sleep_time_ns
+	};
+	nanosleep(&sleep_time, NULL);
 }
 
 void (__attribute__((thiscall))*delay_orig)(uint32_t offset_obj, uint64_t to_delay_ticks) = NULL;
@@ -76,7 +84,11 @@ void __attribute__((thiscall))delay(uint32_t offset_obj, uint64_t to_delay_ticks
 		clock_gettime(CLOCK_MONOTONIC, &now);
 		uint64_t time_since_last_call = (now.tv_nsec - last_call.tv_nsec);
 		if (to_delay_ns > time_since_last_call){
+			#if 1
 			busynanosleep(to_delay_ns - time_since_last_call);		
+			#else
+			normalnanosleep(to_delay_ns - time_since_last_call);
+			#endif
 		}
 		LARGE_INTEGER now_win = {0};
 		QueryPerformanceCounter(&now_win);
